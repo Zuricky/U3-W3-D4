3a83df86b0ddb96a620bf1f7907d54382ac8ffe1
Luca Zuin <zuin.luca1111@gmail.com>
2025-02-20 15:54:47 +0100

    commit 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import ArticleList from "./components/ArticleList";
+import ArticleDetail from "./components/ArticleDetail";
 
-function App() {
-  return <></>;
-}
+const App: React.FC = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<ArticleList />} />
+        <Route path="/article/:id" element={<ArticleDetail />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default App;
